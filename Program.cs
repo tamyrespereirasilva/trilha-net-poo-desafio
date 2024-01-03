6a412907022612dd0d5c5a89af94c2cde76cfc84
Tamyres Silva <108297293+tamyrespereirasilva@users.noreply.github.com>
2024-01-03 13:21:25 -0300

    Desafio resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Play Store");
+Smartphone nokia = new Nokia(numero: "123123", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("App Store");
+Smartphone iphone = new Iphone(numero: "456456", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
