dac72b6fcb9ff24458b009f19036f032c4da8992
Tamyres Silva <108297293+tamyrespereirasilva@users.noreply.github.com>
2024-01-24 01:22:15 -0300

    atualizando nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,19 +1,11 @@
 namespace DesafioPOO.Models
 {
-<<<<<<< HEAD
-=======
-    
->>>>>>> 7535a1472d7fb3288ed5a3b06c29c6ce2b6b8c34
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string modelo, string imei, int memoria, Dictionary<string, string> contatos) : base(numero, modelo, imei, memoria, contatos)
         {
         }
 
-<<<<<<< HEAD
-=======
-        
->>>>>>> 7535a1472d7fb3288ed5a3b06c29c6ce2b6b8c34
         public override void InstalarAplicativo(string nomeApp)
         {
             List<string> appNokia = new List<string> {"Instagram", "Telegram", "Whatsapp"};
